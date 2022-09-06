77e7037dc132fbeedf1895265493a60d25e758f6
Lukas Meinhart <100217325+Drako222@users.noreply.github.com>
2022-09-06 09:25:03 +0200

    First commit

---
+++ site.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+    <?php
+    $name = 'John';
+    $age = 58;
+    $greeting = 'Hello';
+    echo ('<h1>'.$greeting.' Sir '.$name.'</h1>');
+    echo ('<p>You are today '.$age.' years old</p>');
+    $name = 'Paul';
+    echo strtolower('<p>Oh sorry... You are actually '.$name.'</p>');
+    $nameLength = strlen($name);
+    $firstLetter = $name[0];
+    $name[0] = 'M';
+    echo ('<p>My name is now '.$name.' and it is '.$nameLength.' characters long + its first letter used to be '.$firstLetter.'</p>');
+    $age = strval($age);
+    $age = str_replace('5', '6', $age);
+    echo ('<p>New age is '.$age.'</p>');
+    $greeting = substr($greeting, 0,4);
+    echo $greeting;
+
+    echo '<h1>Math</h1>';
+    $number = -120;
+    $number = abs($number);
+    $number += 24;
+    $number = sqrt($number);
+    $number = pow($number, 2);
+    echo $number;
+    ?>
+</body>
+</html>
